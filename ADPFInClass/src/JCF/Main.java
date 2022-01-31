9ecd098168dfe7f93743f2f2c33d544a5dd5cb14
Seth <54418905+Sbennett99@users.noreply.github.com>
2022-01-31 08:44:43 -0600

    update

--- ADPFInClass/src/JCF/Main.java
+++ ADPFInClass/src/JCF/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Test.testList();
+		//Test.testList();
 		Test.testPCompare();
 		Test.testStack();
-		Test.testQueue();
-		Test.testDequeue();
+		//Test.testQueue();
+		//Test.testDequeue();
 
 	}
 
--- ADPFInClass/src/JCF/TestStack.java
+++ ADPFInClass/src/JCF/TestStack.java
@@ -12,7 +12,42 @@ public class TestStack {
         stack.push(5);
 
         System.out.println(stack);
-        System.out.println("test");
+        System.out.println("\nReverse");
+        System.out.print("[");
+        for(int i = stack.size()-1; i>=0; i--){
+            if(i!=0) {
+                System.out.print(stack.get(i) + ", ");
+            }
+            else {
+                System.out.println(stack.get(i) + "]");
+            }
+        }
+
+        System.out.println("Proof Stack was not changed");
+        System.out.println(stack);
+
+
+        System.out.println("\nPopping Item: " + stack.pop());
+
+        System.out.println(stack);
+
+        System.out.println("\nPeeking in the stack: " + stack.peek());
+        // Should Be unchanged
+        System.out.println(stack);
+
+        System.out.println("\nIs the Stack empty: " + stack.empty());
+
+        System.out.println("\nAdding Vs Pushing ?");
+        System.out.println("adding 7");
+        stack.add(7);
+        System.out.println(stack);
+        System.out.println("Pushing 13");
+        stack.push(13);
+        System.out.println(stack);
+
+        System.out.println("Adding can be index specified while pushing is only at the end");
+
+
 
 
     }
